25c88217ab79af2d0f2765be1efc59f3745b555c
stevebeanblue <stevebeanblue@aol.com>
2022-06-07 20:43:28 +0100

    Update models.py

--- pathtoglory/models.py
+++ pathtoglory/models.py
@@ -12,10 +12,23 @@ class QuestLog(models.Model):
     QuestReward = models.TextField()
     QuestProgress = models.TextField()
 
+class Stronghold(models.Model):
+    Name = models.TextField()
+    Barracks = models.TextField()
+    Imposing = models.BooleanField(default=False)
+    Mighty = models.BooleanField(default=False)
+
+class Achievements(models.Model):
+    BattlesFought = models.IntegerField()
+    QuestCompleted = models.IntegerField()
+    VictoriesWon = models.IntegerField()
+    EnemyHereosSlain = models.IntegerField()
+
 class Roster(models.Model):
     Group_id = models.ForeignKey(
         PathToGloryGroup,
-        on_delete=models.CASCADE)
+        on_delete=models.CASCADE
+    )
     PlayerName = models.TextField()
     Faction = models.TextField()
     RealmOfOrigin = models.TextField()
@@ -24,12 +37,12 @@ class Roster(models.Model):
     StartingSize = models.IntegerField()
     User = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE)
+        on_delete=models.CASCADE
+    )
     IsCompleted = models.BooleanField(default=False)
     DateCreated = models.DateField()
     Glory = models.IntegerField(default=0)
-    QuestLog = models.OneToOneField(
+    QuestLog = models.ForeignKey(
         QuestLog,
-        on_delete=models.CASCADE,
-        default=QuestLog.objects.create
+        on_delete=models.CASCADE
     )
