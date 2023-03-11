eb9aad147d19de2268ae6c88c65817044a492fd4
steveNewmanDfT <stevebeanblue@aol.com>
2023-03-11 09:51:33 +0000

    wip

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e5!t0m=@eowwg4x_$g7wdep)8v(gih(mcc+dsb_92zcb0%hok='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
